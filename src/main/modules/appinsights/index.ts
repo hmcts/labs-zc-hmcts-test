9f648e09eb978d543e00d5e9efd13de683511f3d
Scaffolder <scaffolder@backstage.io>
2022-02-23 15:04:56 +0000

    Initial commit

---
+++ src/main/modules/appinsights/index.ts
@@ -0,0 +1,15 @@
+import config from 'config';
+
+const appInsights = require('applicationinsights');
+
+export class AppInsights {
+  enable(): void {
+    if (config.get('appInsights.instrumentationKey')) {
+      appInsights.setup(config.get('appInsights.instrumentationKey')).setSendLiveMetrics(true).start();
+
+      appInsights.defaultClient.context.tags[appInsights.defaultClient.context.keys.cloudRole] =
+        'labs-zc-hmcts';
+      appInsights.defaultClient.trackTrace({ message: 'App insights activated' });
+    }
+  }
+}
